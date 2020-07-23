2f42fdc5e914adb9ac743690bcc3f4db07478b36
A7F <rockthisway05@gmail.com>
2020-07-23 12:28:03 +0200

    swtich to actual config file

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from os.path import realpath, dirname
 import json
 
-cfg_file_path = realpath(dirname(__file__) + "/config/config_test.json")
+cfg_file_path = realpath(dirname(__file__) + "/config/config.json")
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0'}
 statuspage = 'https://magicthegatheringarena.statuspage.io/'
 rotation = 'https://whatsinstandard.com/api/v6/standard.json'
