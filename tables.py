07231328e20de6ac561366da4f49501d6d88ecbc
A7F <rockthisway05@gmail.com>
2020-09-05 16:06:22 +0200

    deleted duplicate code

--- tables.py
+++ tables.py
@@ -4,8 +4,12 @@ from config import config
 
 db = SqliteDatabase(config["database"]["path"])
 
+class BaseModel(Model):
+    class Meta:
+        database = db
 
-class User(Model):
+
+class User(BaseModel):
     user_id = IntegerField(primary_key=True)     # telegram user ID
     group = IntegerField()                  # each user can belong to only one group
     dci = IntegerField(null=True)           # dci wotc gamer number
@@ -17,11 +21,8 @@ class User(Model):
     dci_points = IntegerField(null=True)    # dci planeswalker points
     status = IntegerField(default=0)        # bot navigation based on FSM machine
 
-    class Meta:
-        database = db
 
-
-class Event(Model):
+class Event(BaseModel):
     event_id = IntegerField()
     name = CharField()          # event title
     type = CharField(null=True)          # fnm, draft, constructed, ...
@@ -32,11 +33,8 @@ class Event(Model):
     subs = IntegerField(default=0)       # total subscribers to this event
     winner = IntegerField(null=True)     # player ID who went first
 
-    class Meta:
-        database = db
 
-
-class Round(Model):
+class Round(BaseModel):
     event_id = IntegerField()   # an event id
     round = IntegerField(default=1)      # round number
     player1 = IntegerField(null=True)    # first player ID
@@ -45,11 +43,8 @@ class Round(Model):
     home = IntegerField(null=True)       # player 1 points on 3 rounds
     visitor = IntegerField(null=True)    # player 2 points on 3 rounds (ex: 2-1)
 
-    class Meta:
-        database = db
 
-
-class Code(Model):
+class Code(BaseModel):
     code_id = IntegerField(primary_key=True)
     code = CharField(null=True)     # actual code
     type = CharField(null=True)     # fnm, draft, constructed, ...
@@ -59,13 +54,7 @@ class Code(Model):
     password = BooleanField(default=False)  # a specific word to get this code
     infos = CharField(null=True)  # further infos about this code
 
-    class Meta:
-        database = db
 
-
-class Feed(Model):
+class Feed(BaseModel):
     feed_id = CharField(primary_key=True)
     date = DateTimeField(default=datetime.datetime.today())
-
-    class Meta:
-        database = db
